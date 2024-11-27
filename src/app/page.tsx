7b018d3785d7536ba45e66faafca6a6c753c9306
alirezayoom <alirezayoom@gmail.com>
2024-11-27 15:46:30 +0430

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>Hello world!</div>;
+export default async function Home() {
+  return (
+    <div className="flex justify-center items-center text-neutral-900 dark:text-neutral-50 h-[80vh] w-full font-bold text-3xl">
+      Welcome to Movies
+    </div>
+  );
 }
